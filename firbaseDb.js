6e669f82b690304199bfd501a210c9c3e3c96cde
vytautas-dotcom <tumas.vytautas@gmail.com>
2026-05-19 09:07:25 +0300

    refactor

---
+++ firbaseDb.js
@@ -0,0 +1,138 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOKsBulhwPJWTmg5OKgYuITCTTjrp7k40",
+  authDomain: "minimemorygame-e244f.firebaseapp.com",
+  projectId: "minimemorygame-e244f",
+  storageBucket: "minimemorygame-e244f.firebasestorage.app",
+  messagingSenderId: "981391228420",
+  appId: "1:981391228420:web:93020dc0d18502e6deff51",
+  measurementId: "G-GQH6T1RSPR",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function loadTop10(playerId) {
+  const topContainer = document.getElementById("top10");
+  topContainer.innerHTML = "";
+
+  const q = query(
+    collection(db, "players"),
+    orderBy("score", "desc"),
+    limit(10),
+  );
+
+  const querySnapshot = await getDocs(q);
+  loadMyRank(querySnapshot);
+
+  let position = 1;
+
+  querySnapshot.forEach((docSnap) => {
+    const data = docSnap.data();
+
+    const row = document.createElement("div");
+    row.classList.add("top-row");
+
+    if (position === 1) row.classList.add("gold");
+    if (position === 2) row.classList.add("silver");
+    if (position === 3) row.classList.add("bronze");
+
+    if (docSnap.id === playerId) {
+      row.classList.add("my-row");
+    }
+
+    let medal = position;
+
+    if (position === 1) medal = "🥇";
+    if (position === 2) medal = "🥈";
+    if (position === 3) medal = "🥉";
+
+    row.innerHTML = `
+      <p class="top-place">${medal}</p>
+      <p class="top-place">${data.nickname}</p>
+      <p class="top-score">${data.score}</p>
+      <p class="top-date">${new Intl.DateTimeFormat("lt-LT", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+      }).format(new Date(data.updatedAt))}</p>
+    `;
+
+    topContainer.appendChild(row);
+
+    if (docSnap.id === playerId) {
+      setTimeout(() => {
+        row.classList.add("flash-row");
+
+        if (position > 3) {
+          row.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+          });
+        }
+      }, 300);
+    }
+
+    position++;
+  });
+}
+
+export async function loadMyRank(querySnapshot) {
+  const rankEl = document.getElementById("myRank");
+
+  let position = 1;
+  let myPosition = null;
+
+  querySnapshot.forEach((docSnap) => {
+    if (docSnap.id === playerId) {
+      myPosition = position;
+    }
+    position++;
+  });
+
+  if (!myPosition) {
+    rankEl.textContent = "";
+    return await getScore(playerId);
+  }
+
+  if (myPosition === 1) {
+    rankEl.textContent = "🥇";
+  } else if (myPosition === 2) {
+    rankEl.textContent = "🥈";
+  } else if (myPosition === 3) {
+    rankEl.textContent = "🥉";
+  } else {
+    rankEl.textContent = `#${myPosition}`;
+  }
+}
+
+async function getScore(playerId) {
+  const docRef = doc(db, "players", playerId);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    return docSnap.data();
+  }
+}
+
+export async function saveScore(score, playerId, nickname) {
+  await setDoc(doc(db, "players", playerId), {
+    score: score,
+    nickname: nickname,
+    updatedAt: new Date().toISOString(),
+  });
+}
--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc,
-  collection,
-  getDocs,
-  query,
-  orderBy,
-  limit,
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { loadTop10, loadMyRank, saveScore } from "./firbaseDb.js";
+import { generateNumber } from "./utilities.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAOKsBulhwPJWTmg5OKgYuITCTTjrp7k40",
-  authDomain: "minimemorygame-e244f.firebaseapp.com",
-  projectId: "minimemorygame-e244f",
-  storageBucket: "minimemorygame-e244f.firebasestorage.app",
-  messagingSenderId: "981391228420",
-  appId: "1:981391228420:web:93020dc0d18502e6deff51",
-  measurementId: "G-GQH6T1RSPR",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
+const nicknameScreen = document.getElementById("nicknameScreen");
+const nicknameInput = document.getElementById("nicknameInput");
+const saveNameBtn = document.getElementById("saveNameBtn");
+const gameScreen = document.getElementById("gameScreen");
 const numberBox = document.getElementById("numberBox");
 const answerInput = document.getElementById("answerInput");
 const checkBtn = document.getElementById("checkBtn");
@@ -31,35 +12,57 @@ const levelEl = document.getElementById("level");
 const message = document.getElementById("message");
 
 let playerId = localStorage.getItem("memoryPlayerId");
+let playerName = localStorage.getItem("memoryPlayerNickname");
 let score = 4;
 let started = false;
 let currentNumber = "";
+let nickname = undefined;
 
-function generateNumber(length) {
-  let num = "";
-  for (let i = 0; i < length; i++) {
-    num += Math.floor(Math.random() * 10);
-  }
-  return num;
-}
+async function init() {
 
-async function startRound() {
   if (!playerId) {
+    nicknameScreen.style.display = "block";
+    gameScreen.style.display = "none";
+  }else{
+    nicknameScreen.style.display = "none";
+    gameScreen.style.display = "block";
+    await loadTop10(playerId);
+
+    await startRound();
+  }
+
+  if(nickname){
+    nicknameScreen.style.display = "none";
+    gameScreen.style.display = "block";
+
     playerId = crypto.randomUUID();
     localStorage.setItem("memoryPlayerId", playerId);
+    localStorage.setItem("memoryPlayerNickname", nickname);
+
+    await startRound();
   }
+}
 
-  if (!started) {
-    const savedScore = await getScore(playerId);
 
-    if (savedScore && savedScore >= 4) {
-      score = savedScore;
-    } else {
-      score = 4;
-    }
 
-    started = true;
-  }
+async function startRound() {
+  // if (!playerId) {
+  //   playerId = crypto.randomUUID();
+  //   localStorage.setItem("memoryPlayerId", playerId);
+  // }
+
+  // if (!started) {
+  //   const savedScore = await getScore(playerId)[0];
+  //   nickname = await getScore(playerId)[1];
+
+  //   if (savedScore && savedScore >= 4) {
+  //     score = savedScore;
+  //   } else {
+  //     score = 4;
+  //   }
+
+  //   started = true;
+  // }
 
   answerInput.value = "";
   answerInput.disabled = true;
@@ -79,115 +82,41 @@ async function startRound() {
   }, 2000);
 }
 
-// async function loadTop10() {
-//   const topContainer = document.getElementById("top10");
-//   topContainer.innerHTML = "";
 
-//   const q = query(
-//     collection(db, "players"),
-//     orderBy("score", "desc"),
-//     limit(10)
-//   );
 
-//   const querySnapshot = await getDocs(q);
+checkBtn.addEventListener("click", () => {
+  const answer = answerInput.value.trim();
 
-//   let position = 1;
-
-//   querySnapshot.forEach((docSnap) => {
-//     const data = docSnap.data();
-
-//     const row = document.createElement("p");
-//     row.textContent = `${position}. ${data.score} - ${new Date(data.updatedAt).toLocaleString()}`;
-
-//     topContainer.appendChild(row);
-
-//     position++;
-//   });
-// }
-
-async function loadTop10() {
-  const topContainer = document.getElementById("top10");
-  topContainer.innerHTML = "";
-
-  const q = query(
-    collection(db, "players"),
-    orderBy("score", "desc"),
-    limit(10),
-  );
-
-  const querySnapshot = await getDocs(q);
-
-  let position = 1;
-
-  querySnapshot.forEach((docSnap) => {
-    const data = docSnap.data();
-
-    const row = document.createElement("div");
-    row.classList.add("top-row");
-
-    if (position === 1) row.classList.add("gold");
-    if (position === 2) row.classList.add("silver");
-    if (position === 3) row.classList.add("bronze");
-
-    let medal = position;
-
-    if (position === 1) medal = "🥇";
-    if (position === 2) medal = "🥈";
-    if (position === 3) medal = "🥉";
-
-    row.innerHTML = `
-      <p class="top-place">${medal}</p>
-      <p class="top-score">${data.score}</p>
-      <p class="top-date">${new Intl.DateTimeFormat("lt-LT", {
-        year: "numeric",
-        month: "numeric",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-      }).format(new Date(data.updatedAt))}</p>
-    `;
-
-    topContainer.appendChild(row);
-
-    position++;
-  });
-}
-
-async function loadMyRank() {
-  const rankEl = document.getElementById("myRank");
-
-  const q = query(
-    collection(db, "players"),
-    orderBy("score", "desc")
-  );
-
-  const querySnapshot = await getDocs(q);
-
-  let position = 1;
-  let myPosition = null;
-
-  querySnapshot.forEach((docSnap) => {
-    if (docSnap.id === playerId) {
-      myPosition = position;
-    }
-    position++;
-  });
-
-  if (!myPosition) {
-    rankEl.textContent = "";
-    return;
-  }
-
-  if (myPosition === 1) {
-    rankEl.textContent = "🥇";
-  } else if (myPosition === 2) {
-    rankEl.textContent = "🥈";
-  } else if (myPosition === 3) {
-    rankEl.textContent = "🥉";
+  if (answer === currentNumber) {
+    message.textContent = "Teisingai";
+    score++;
+    setTimeout(startRound, 1000);
   } else {
-    rankEl.textContent = `#${myPosition}`;
+    message.textContent = `Baigta. Rezultatas: ${score - 1}`;
+    saveScore(score - 1, playerId, nickname);
+    setTimeout(() => {
+      loadTop10();
+    }, 1000);
+
+    score = 4;
+    started = false;
+
+    setTimeout(startRound, 3000);
   }
-}
+});
+
+saveNameBtn.addEventListener("click", async () => {
+  const value = nicknameInput.value.trim();
+
+  if (!value) return;
+  nickname = value;
+
+  init();
+});
+
+await init();
+
+
 
 // async function getScore(playerId) {
 //   try {
@@ -205,16 +134,6 @@ async function loadMyRank() {
 //   }
 // }
 
-async function getScore() {
-  const docRef = doc(db, "players", playerId);
-  const docSnap = await getDoc(docRef);
-
-  if (docSnap.exists()) {
-    return docSnap.data().score;
-  }
-
-  return 4;
-}
 
 // function saveScore(score) {
 //   const formData = new FormData();
@@ -229,36 +148,3 @@ async function getScore() {
 //     body: formData
 //   });
 // }
-
-async function saveScore(score) {
-  await setDoc(doc(db, "players", playerId), {
-    score: score,
-    updatedAt: new Date().toISOString(),
-  });
-}
-
-checkBtn.addEventListener("click", () => {
-  const answer = answerInput.value.trim();
-
-  if (answer === currentNumber) {
-    message.textContent = "Teisingai";
-    score++;
-    setTimeout(startRound, 1000);
-  } else {
-    message.textContent = `Baigta. Rezultatas: ${score - 1}`;
-    saveScore(score - 1);
-    setTimeout(() => {
-  loadTop10();
-  loadMyRank();
-}, 1000);
-
-    score = 4;
-    started = false;
-
-    setTimeout(startRound, 3000);
-  }
-});
-
-loadMyRank();
-loadTop10();
-startRound();
