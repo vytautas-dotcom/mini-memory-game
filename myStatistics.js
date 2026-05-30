da2af1158ad70428c4a567027411ae690fb3cb3d
Giedre <giedre.klima@gmail.com>
2026-05-30 20:40:29 +0300

    my statistics style, add data, font family for all

--- myStatistics.js
+++ myStatistics.js
@@ -1,4 +1,4 @@
-import {getScore} from "./firbaseDb.js"
+import { getScore } from "./firbaseDb.js"
 
 let playerId = localStorage.getItem("memoryPlayerId");
 let playerName = localStorage.getItem("memoryPlayerNickname");
@@ -6,24 +6,36 @@ let playerName = localStorage.getItem("memoryPlayerNickname");
 let data = await getScore(playerId);
 
 // FIREBASE DUOMENYS
-    const playerData = {
-      nickname: playerName,
-      startResult: data.startResult,
-      bestResult: data.bestResult,
-      lastResult: data.lastResult
-    };
+const playerData = {
+  nickname: playerName,
+  startResult: data.startResult,
+  bestResult: data.bestResult,
+  lastResult: data.lastResult,
+  startDate: data.startDate,
+  lastDate: data.lastDate
+};
 
-    // UI
-    document.getElementById("nickname").textContent =
-      playerData.nickname;
+// UI
+document.getElementById("nickname").textContent =
+  playerData.nickname;
 
-    document.getElementById("startResult").textContent =
-      playerData.startResult;
+document.getElementById("startResult").textContent =
+  playerData.startResult;
 
-    document.getElementById("bestResult").textContent =
-      playerData.bestResult;
+document.getElementById("bestResult").textContent =
+  playerData.bestResult;
 
-    document.getElementById("lastResult").textContent =
-      playerData.lastResult;
+document.getElementById("lastResult").textContent =
+  playerData.lastResult;
 
-   
+document.getElementById("startDate").textContent =
+  playerData.startDate.slice(0, 10);
+
+document.getElementById("startHour").textContent =
+  playerData.startDate.slice(11, 16);
+
+document.getElementById("lastDate").textContent =
+  playerData.lastDate.slice(0, 10);
+
+document.getElementById("lastHour").textContent =
+  playerData.lastDate.slice(11, 16);
