45d2d526b1eb99f00b4f9c702c456953c74d9288
Giedre <giedre.klima@gmail.com>
2026-05-31 14:27:46 +0300

    main btns link, top10 styles

--- top10.js
+++ top10.js
@@ -3,6 +3,8 @@ import { loadTop10 } from "./firbaseDb.js";
 
 let playerId = localStorage.getItem("memoryPlayerId");
 let playerName = localStorage.getItem("memoryPlayerNickname");
+// UI
+document.getElementById("nickname").textContent = playerName;
 
 async function init() {
     await loadTop10(playerId);
